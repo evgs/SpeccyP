ed754728208d946af7f203670eb3de0a7817d899
Eugeniy Stahov <eugene.stahov@gmail.com>
2026-05-14 23:50:12 +0500

    batch build release uf2 for m1p1 and m1p2

--- cleanbuild.sh
+++ cleanbuild.sh
@@ -1,4 +1,11 @@
 cd build
+
 rm -r *
-cmake ..
+cmake -DPICO_BOARD=pico ..
 cmake --build . --parallel
+cp *.uf2 ../bin
+
+rm -r *
+cmake -DPICO_BOARD=pico2 ..
+cmake --build . --parallel
+cp *.uf2 ../bin
